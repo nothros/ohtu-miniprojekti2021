c7d8147dd15da21c2adffefcc60e804a490d1251
Erika Horros-Wallin <erika.horros@gmail.com>
2021-11-23 18:15:04 +0200

    Add javafx depencies and first javafx helloworld

--- app/src/main/java/kurssikirjahylly/App.java
+++ app/src/main/java/kurssikirjahylly/App.java
@@ -1,10 +1,29 @@
 package kurssikirjahylly;
 
 import tietokantaDemo.tietokantaDemo;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
-public class App {
+public class App extends Application{
     public String getGreeting() {
         return "Tervehdys. Ilman minua eka testi kaatuu.";
+    }
+
+    // Olen täällä javafx:ää varten. ja keulin nyt kaiken muun edelle
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello world Application");
+        primaryStage.setWidth(300);
+        primaryStage.setHeight(200);
+    
+        Label helloWorldLabel = new Label("Hello world!");
+        helloWorldLabel.setAlignment(Pos.CENTER);
+        Scene primaryScene = new Scene(helloWorldLabel);
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
     }
 
 /*
@@ -12,6 +31,7 @@ Luokka luo tietokannan /app/test.db ja lisää siihen kaksi oliota.
 Poista k. tiedosto jos haluat nollata.
 */
     public static void main(String[] args) {
+        Application.launch(args);
         System.out.println(new App().getGreeting());
         tietokantaDemo td = new tietokantaDemo();
         td.createNewDatabase("test");
