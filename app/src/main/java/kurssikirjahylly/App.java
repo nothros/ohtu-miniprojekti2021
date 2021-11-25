97d4071991a55f594e9596f9b1cafd48969fe040
Erika Horros-Wallin <erika.horros@gmail.com>
2021-11-25 20:58:08 +0200

    Add AppUi

--- app/src/main/java/kurssikirjahylly/App.java
+++ app/src/main/java/kurssikirjahylly/App.java
@@ -8,6 +8,7 @@ public class App {
     public String getGreeting() {
         return "Tervehdys. Ilman minua eka testi kaatuu.";
     }
+    
 
 /*
 Luokka luo tietokannan /app/test.db ja lisää siihen kaksi oliota.
@@ -15,7 +16,7 @@ Poista k. tiedosto jos haluat nollata.
 */
     public static void main(String[] args) {
         AppUi.main(args);
-
+        
         System.out.println(new App().getGreeting());
         tietokantaDemo td = new tietokantaDemo();
         td.createNewDatabase("test");
