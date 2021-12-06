fd3b1aef088b23805f9f3fdc92270e2b6891e41f
juhkure <johannes.pyykonen@helsinki.fi>
2021-12-06 02:01:47 +0200

    added tests for libraryobjectdao

--- app/src/test/java/database/LibraryObjectDaoTest.java
+++ app/src/test/java/database/LibraryObjectDaoTest.java
@@ -1,4 +1,3 @@
-
 package database;
 
 import org.junit.Test;
@@ -13,75 +12,88 @@ import static org.junit.Assert.*;
 import java.sql.SQLException;
 import java.util.List;
 
-
 public class LibraryObjectDaoTest {
-    
+
     private LibraryObjectDAO dao;
-    
+
     private String testDB;
+
     /**
-    This method is performed before every test. It creates an empty db into memory.
-    */
+     * This method is performed before every test. It creates an empty db into
+     * memory.
+     */
     @Before
-    public void init() throws SQLException{
+    public void init() throws SQLException {
         testDB = "daotest.db";
         dao = new LibraryObjectDAO(testDB);
         dao.createNewTable();
     }
-    
+
     @Test
-    public void testEmptyDatabaseSize(){
+    public void testEmptyDatabaseSize() {
         assertEquals(0, dao.getAll().size());
     }
-    
+
     @Test
-    public void testDatabaseInsertion(){
-        LibraryObject book1 = new LibraryObject("book", "Weapons of Math Destruction","Cathy O'Neil", "12345678910", null, null);
-        LibraryObject book2 = new LibraryObject("book", "Clean code","Joku Muu", "11122233344", null, null);
+    public void testDatabaseInsertion() {
+        LibraryObject book1 = new LibraryObject("book", "Weapons of Math Destruction", "Cathy O'Neil", "12345678910", null, null);
+        LibraryObject book2 = new LibraryObject("book", "Clean code", "Joku Muu", "11122233344", null, null);
         dao.insertLibrary(book1);
         dao.insertLibrary(book2);
         assertEquals(2, dao.getAll().size());
     }
-    
+
     @Test
-    public void testReadAfterInsertion(){
-        LibraryObject book2 = new LibraryObject("book", "Clean code","Joku Muu", "111222", null, null);
+    public void testReadAfterInsertion() {
+        LibraryObject book2 = new LibraryObject("book", "Clean code", "Joku Muu", "111222", null, null);
 
         dao.insertLibrary(book2);
         List<LibraryObject> objs = dao.getAll();
         assertEquals(1, objs.size());
-        
+
         LibraryObject book = objs.get(0);
         assertEquals("Clean code", book.getTitle());
     }
-    
+
     @Test
-    public void testCourseInsertion(){
+    public void testIdReturnWithIsbn() {
+        LibraryObject book = new LibraryObject("book", "Test Book", "Test Writer", "44433322211", null, null);
+        dao.insertLibrary(book);
+
+        assertEquals(1, dao.getLibraryId("44433322211"));
+    }
+
+    @Test
+    public void testCourseInsertion() {
         dao.insertCourse(new CourseObject("Kurssi1"));
         dao.insertCourse(new CourseObject("Kurssi2"));
         assertEquals(1, dao.getCourseId("Kurssi1"));
         assertEquals(2, dao.getCourseId("Kurssi2"));
     }
-    
+
     @Test
-    public void testIsUniqueCourse(){
+    public void testIsUniqueCourse() {
         dao.insertCourse(new CourseObject("Kurssi1"));
         assertTrue(dao.isUniqueCourse("Kurssi2"));
         assertFalse(dao.isUniqueCourse("Kurssi1"));
     }
-    
+
     @Test
-    public void testDeleteTable(){
-        LibraryObject book2 = new LibraryObject("book", "Clean code","Joku Muu", "111222", null, null);
+    public void testNullIsbnReturnsFalse() {
+        assertEquals(false, dao.isValidISBN(null));
+    }
+
+    @Test
+    public void testDeleteTable() {
+        LibraryObject book2 = new LibraryObject("book", "Clean code", "Joku Muu", "111222", null, null);
         dao.insertLibrary(book2);
         dao.deleteTable();
         assertEquals(0, dao.getAll().size());
     }
-    
+
     @After
-    public void tearDown()throws SQLException{
+    public void tearDown() throws SQLException {
         dao.deleteDatabase(testDB);
     }
-   
-    
+
 }
